90a9d3c35733d11b3ade6729d28dc8ac0e1da488
Sable Seyler <sable@seyleri.us>
2021-01-11 17:52:13 -0600

    Update spotify-ripper nix expression path

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,7 @@
 { pkgs }:
 
 rec {
-  spotify-ripper = pkgs.callPackage ../tools/misc/spotify-ripper {
+  spotify-ripper = pkgs.callPackage ../tools/collectors/spotify-ripper {
     # NOTE: Not available in 20.03. Specifying it this way lets me cheat the
     # build auto-failing on 20.03 because of the attribute not existing.
     inherit (pkgs) fdk-aac-encoder;
