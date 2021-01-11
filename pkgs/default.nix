2b38ed4908eb852ccea7d905ae88774984bb128d
Sable Seyler <sable@seyleri.us>
2021-01-11 17:48:57 -0600

    Remove unnecessary attribute update to a void var

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,6 +5,6 @@ rec {
     # NOTE: Not available in 20.03. Specifying it this way lets me cheat the
     # build auto-failing on 20.03 because of the attribute not existing.
     inherit (pkgs) fdk-aac-encoder;
-    python2Packages = pkgs.python3Packages // python3Packages.overlay;
+    python2Packages = pkgs.python3Packages;
   };
 }
