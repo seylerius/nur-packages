6e7e706da39ce31c2491b80001be6da3d18fed4f
Sable Seyler <sable@seyleri.us>
2022-07-27 14:40:56 -0700

    Abandon unused works in progress

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,16 +1,5 @@
 { pkgs }:
 
 rec {
-  python3Packages = pkgs.python3Packages // {
-    desktop-notify = pkgs.callPackage ./python/desktop-notify/default.nix {
-      inherit python3Packages;
-    };
 
-    xerox =
-      pkgs.callPackage ./python/xerox/default.nix { inherit python3Packages; };
-  };
-
-  keeprofi = pkgs.callPackage ./gui/launcher/rofi/keeprofi.nix {
-    inherit python3Packages;
-  };
 }
